3ab38229b81ca3a69e07d22bec77576c255db20d
SviluppoSoftware <svilupposoftware@scfgroup.it>
2021-03-29 12:01:36 +0200

    Initial commit

---
+++ config/voyager-config.php
@@ -0,0 +1,28 @@
+<?php
+
+// This is the config for Voyager-Config
+
+return [
+
+  // the config path where the subfolder with Voyager Export data should be put
+  'path' => app_path() . '/../config/',
+
+  // the name of the subfolder where the Voyager Export data should be put
+  'folder' => 'voyager',
+
+  // list of the tables which belong to Voyager to export
+  'tables' => [
+    'data_rows',
+    'data_types',
+    'menus',
+    'menu_items',
+    'permissions',
+    'permission_role',
+    'roles',
+    'settings',
+    'translations',
+  ],   //List of tables that will ignore when lunch the command export
+    'defaultSkip' =>[
+    ]
+
+];
